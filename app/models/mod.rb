eb5c1ad4f10488fd3d98f3db3a5cdae3be38a7d3
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-03-15 15:13:24 -0500

    Added Dynamic mod editing.

--- app/models/mod.rb
+++ app/models/mod.rb
@@ -4,4 +4,7 @@ class Mod < ActiveRecord::Base
   has_attached_file :mod,                                                                                                                  
       :path => ":rails_root/public/system/:attachment/:id/:filename",                                                                    
       :url => "/system/:attachment/:id/:filename"
+  def self.downloadlist
+    self.all
+  end
 end
